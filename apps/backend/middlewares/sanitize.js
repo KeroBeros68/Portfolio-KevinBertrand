11e73e3f2ca5112974bc198aa2631ddee48674cd
KeroBeros68 <kevinbertrand68.pro@gmail.com>
2025-05-26 16:16:00 +0200

    erreur

--- apps/backend/middlewares/sanitize.js
+++
@@ -1,8 +0,0 @@
-const sanitize = require('mongo-sanitize');
-
-module.exports = (req, res, next) => {
-    req.body = sanitize(req.body);
-    req.query = sanitize(req.query);
-    req.params = sanitize(req.params);
-    next();
-};
