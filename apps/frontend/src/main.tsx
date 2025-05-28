b669eae8e7881eaeb7f4695f305e9fa271cec7dd
KeroBeros68 <kevinbertrand68.pro@gmail.com>
2025-05-28 11:34:38 +0200

    passage en heroUI

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Router from './router/router.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import App from "./App.tsx";
+import { Provider } from "./provider.tsx";
+import "@/styles/globals.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <Router />
+      <Provider>
+        <App />
+      </Provider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
