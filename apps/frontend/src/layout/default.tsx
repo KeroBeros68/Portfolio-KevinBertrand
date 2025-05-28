af64c1b622597cd8be401291309edc4cc655cd5f
KeroBeros68 <kevinbertrand68.pro@gmail.com>
2025-05-28 03:20:04 +0200

    Amélioration footer

--- apps/frontend/src/layout/default.tsx
+++ apps/frontend/src/layout/default.tsx
@@ -1,26 +1,28 @@
 import packageJson from '../../package.json'
 
-//import logo from '@/assets/logo-horizontal-grey.png'
+import logo from '../assets/footerGreyLogo.svg'
 
 const version = packageJson.version
 
 export default function DefaultLayout({
-  banner,
   children,
 }: {
-  banner?: React.ReactNode
   children: React.ReactNode
 }) {
   return (
     <div className='relative flex h-screen flex-col'>
       {/* <Navbar /> */}
       <main className='flex-grow'>
-        {banner}
         <div className='container mx-auto max-w-screen-2xl px-6'>
           {children}
         </div>
       </main>
-      <footer className='flex min-h-[250px] w-full flex-col items-center justify-center gap-2 py-3'>
+      <footer className='bg-neutral flex min-h-[20%] w-full flex-col items-center justify-center border-t-2 border-emerald-950 pb-7 text-amber-50'>
+        <img
+          alt='logo de Kévin BERTRAND - Développeur web'
+          className='max-h-[10rem]'
+          src={logo}
+        />
         <a
           className='flex items-center gap-1 text-current'
           href='https://daisyui.com/'
@@ -28,12 +30,7 @@ export default function DefaultLayout({
           <span className='text-default-600'>Powered by</span>
           <p className='text-primary'>daisyUI</p>
         </a>
-        <img
-          alt='logo de Kévin BERTRAND - Développeur web'
-          className='max-h-[48px]'
-          //src={logo}
-        />
-        <span className='text-default-400'>version: {version} - Mars 2025</span>
+        <span>version: {version} - Mars 2025</span>
       </footer>
     </div>
   )
