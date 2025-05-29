e437e180105bbdccc8bdab12d8cda9f6de31e35f
KeroBeros68 <kevinbertrand68.pro@gmail.com>
2025-05-29 18:47:26 +0200

    Début de la navbar

--- apps/frontend/src/components/navbar.tsx
+++ apps/frontend/src/components/navbar.tsx
@@ -1,7 +1,4 @@
-import { Button } from "@heroui/button";
-import { Kbd } from "@heroui/kbd";
-import { Link } from "@heroui/link";
-import { Input } from "@heroui/input";
+import { Link } from '@heroui/link'
 import {
   Navbar as HeroUINavbar,
   NavbarBrand,
@@ -9,66 +6,40 @@ import {
   NavbarItem,
   NavbarMenuToggle,
   NavbarMenu,
-  NavbarMenuItem,
-} from "@heroui/navbar";
-import { link as linkStyles } from "@heroui/theme";
-import clsx from "clsx";
+} from '@heroui/navbar'
+import { link as linkStyles } from '@heroui/theme'
+import { Image } from '@heroui/image'
+import clsx from 'clsx'
 
-import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
-} from "@/components/icons";
-import { Logo } from "@/components/icons";
+import { siteConfig } from '@/config/site'
+import { ThemeSwitch } from '@/components/theme-switch'
+import { TwitterIcon, GithubIcon } from '@/components/icons'
+import Logo from '@/assets/logo/LogoKevin.svg'
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
-    <HeroUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand className="gap-3 max-w-fit">
+    <HeroUINavbar isBordered shouldHideOnScroll maxWidth='xl' position='sticky'>
+      <NavbarContent className='' justify='start'>
+        <NavbarBrand className='max-w-fit gap-3'>
           <Link
-            className="flex justify-start items-center gap-1"
-            color="foreground"
-            href="/"
+            className='flex items-center justify-start gap-1'
+            color='foreground'
+            href='/'
           >
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <Image alt='logo de Kevin Bertrand' src={Logo} width={300} />
           </Link>
         </NavbarBrand>
-        <div className="hidden lg:flex gap-4 justify-start ml-2">
+      </NavbarContent>
+      <NavbarContent className='sm:w-1/3' justify='center'>
+        <div className='ml-2 hidden justify-start gap-4 sm:flex'>
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
               <Link
                 className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
+                  linkStyles({ color: 'foreground' }),
+                  'data-[active=true]:text-primary data-[active=true]:font-medium',
                 )}
-                color="foreground"
+                color='foreground'
                 href={item.href}
               >
                 {item.label}
@@ -78,67 +49,44 @@ export const Navbar = () => {
         </div>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.twitter} title="Twitter">
-            <TwitterIcon className="text-default-500" />
+      <NavbarContent className='hidden w-1/3 sm:flex' justify='end'>
+        <NavbarItem className='hidden gap-2 sm:flex'>
+          <Link isExternal href={siteConfig.links.twitter} title='Twitter'>
+            <TwitterIcon className='text-default-500' />
           </Link>
-          <Link isExternal href={siteConfig.links.discord} title="Discord">
-            <DiscordIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.github} title="GitHub">
-            <GithubIcon className="text-default-500" />
+          <Link isExternal href={siteConfig.links.github} title='GitHub'>
+            <GithubIcon className='text-default-500' />
           </Link>
           <ThemeSwitch />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
-          >
-            Sponsor
-          </Button>
-        </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+      <NavbarContent className='basis-1 pl-4 sm:hidden' justify='end'>
         <Link isExternal href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
+          <GithubIcon className='text-default-500' />
         </Link>
         <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
+        <div className='mx-4 mt-2 flex flex-col gap-2'>
+          {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href}>
               <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
+                className={clsx(
+                  linkStyles({ color: 'foreground' }),
+                  'data-[active=true]:text-primary data-[active=true]:font-medium',
+                )}
+                color='foreground'
+                href={item.href}
               >
                 {item.label}
               </Link>
-            </NavbarMenuItem>
+            </NavbarItem>
           ))}
         </div>
       </NavbarMenu>
     </HeroUINavbar>
-  );
-};
+  )
+}
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-import App from "./App.tsx";
-import { Provider } from "./provider.tsx";
-import "@/styles/globals.css";
+import Router from './router/router.tsx'
+import { Provider } from './provider.tsx'
+import '@/styles/globals.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
-        <App />
+        <Router />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
-);
+)
